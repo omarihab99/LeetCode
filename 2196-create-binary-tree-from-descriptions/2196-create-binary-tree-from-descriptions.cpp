d4ef51aa8c8e935b27929b54b13460fef0cf2fbb
omarihab99 <47930363+omarihab99@users.noreply.github.com>
2023-03-14 23:13:17 +0200

    Time: 1262 ms (26.74%), Space: 291.2 MB (42.42%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        map<int,TreeNode*> m;
+        unordered_set<int> ch;
+        unordered_set<int> pr;
+        for(int i=0;i<descriptions.size();i++){
+            int p = descriptions[i][0];
+            int c = descriptions[i][1];
+            int l = descriptions[i][2];
+            if(m.find(p) == m.end())
+                m[p] = new TreeNode(p);
+            if(m.find(c) == m.end())
+                m[c] = new TreeNode(c);
+            if(l==1) m[p]->left=m[c];
+            else m[p]->right=m[c];
+            ch.emplace(c);
+            pr.emplace(p);
+            if(ch.find(p)!=ch.end()) pr.erase(p);
+            if(pr.find(c)!=pr.end()) pr.erase(c);
+        }
+        int root = *pr.begin();
+        return m[root];
+        
+    }
+};
