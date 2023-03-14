f6e59c011cdac036ace9ff6f82851ab28ef42434
omarihab99 <47930363+omarihab99@users.noreply.github.com>
2023-03-14 23:25:34 +0200

    Time: 1414 ms (12.60%), Space: 319.8 MB (11.96%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -12,26 +12,51 @@
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        map<int,TreeNode*> m;
-        unordered_set<int> ch;
-        unordered_set<int> pr;
-        for(int i=0;i<descriptions.size();i++){
-            int p = descriptions[i][0];
-            int c = descriptions[i][1];
-            int l = descriptions[i][2];
-            if(m[p]==0)
-                m[p] = new TreeNode(p);
-            if(m[c]==0)
-                m[c] = new TreeNode(c);
-            if(l==1) m[p]->left=m[c];
-            else m[p]->right=m[c];
-            ch.emplace(c);
-            pr.emplace(p);
-            if(ch.find(p)!=ch.end()) pr.erase(p);
-            if(pr.find(c)!=pr.end()) pr.erase(c);
-        }
-        int root = *pr.begin();
-        return m[root];
+        map<int, TreeNode*> mp;
+    for (auto it : descriptions) {
+      TreeNode* parentNode, * childNode;
+
+      // Finding the parent Node
+      if (mp.find(it[0]) != mp.end()) {
+        parentNode = mp[it[0]];
+      }
+      else {
+        parentNode = new TreeNode(it[0]);
+        mp[it[0]] = parentNode;
+      }
+
+      // Finding the child Node
+      if (mp.find(it[1]) != mp.end()) {
+        childNode = mp[it[1]];
+      }
+      else {
+        childNode = new TreeNode(it[1]);
+        mp[it[1]] = childNode;
+      }
+
+      // Making the Edge Between parent and child Node
+      if (it[2] == 1) {
+        parentNode->left = childNode;
+      }
+      else {
+        parentNode->right = childNode;
+      }
+    }
+
+    // Store the childNode 
+    map<int, int> storeChild;
+    for (auto it : descriptions) {
+      storeChild[it[1]] = 1;
+    }
+    // Find the root of the Tree
+    TreeNode* root;
+    for (auto it : descriptions) {
+      if (storeChild.find(it[0]) == storeChild.end()) {
+        root = mp[it[0]];
+      }
+    }
+    return root;
+
         
     }
 };
