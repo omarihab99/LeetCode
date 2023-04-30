27b58427d33793ceab300db63d93bbbd5eeca7ad
omarihab99 <47930363+omarihab99@users.noreply.github.com>
2023-04-30 07:27:08 +0200

    Time: 0 ms (100.00%), Space: 6.1 MB (14.88%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0) return false;
-        return ((n==1) || (n%2==0 && isPowerOfTwo(n/2)));
+        if(n<=0) return false;
+        return ((n&n-1) == 0);
     }
 };
