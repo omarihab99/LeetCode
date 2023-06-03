485b21518cbe6f6659b7c7f903a4147d43916fc9
omarihab99 <47930363+omarihab99@users.noreply.github.com>
2023-06-03 22:11:55 +0300

    Time: 51 ms (71.99%), Space: 27.2 MB (7.64%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -3,16 +3,15 @@ public:
     bool validPalindrome(string s) {
         int left=0;
         int right = s.size()-1;
-        int count =0;
-        while(left<right){
-            if(s[left] == s[right]){left++;right--;}
-            else return test(s, left+1, right) || test(s, left, right-1);     
-        }
-        return true;
+        int count =0;       
+        return test(s, left,right,count);
     }
-    bool test(string s, int left, int right){
+    bool test(string s, int left, int right, int count){
         while(left<right){
             if(s[left] == s[right]){left++;right--;}
+            else if(count<1){
+                return test(s, left+1, right, count+1) || test(s,left, right-1, count+1);
+            }
             else return false;
         }
         return true;
