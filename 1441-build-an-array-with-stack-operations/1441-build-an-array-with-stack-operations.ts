f79f806c53e5a8bb56ebef366e277887e270f78c
omarihab99 <47930363+omarihab99@users.noreply.github.com>
2023-07-15 20:56:54 +0300

    Time: 60 ms (85.71%), Space: 44.8 MB (14.29%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.ts
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.ts
@@ -3,14 +3,12 @@ function buildArray(target: number[], n: number): string[] {
     let stack : number[] = [];
     let j=0;
     for(let i=1; i<=n ;i++){
-        stack.push(i);
         res.push("Push");
         if(i===target[j]){
             if(j===target.length-1) break;
             j++;
         }
         else{
-            stack.pop();
             res.push("Pop");
         }
         
