32946de501b1d387b75cf5a3da8bdc751804e995
Omar Ihab <47930363+omarihab99@users.noreply.github.com>
2023-09-18 23:31:26 +0300

    Time: 4 ms (100%), Space: 63.2 MB (43.74%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,43 +1,24 @@
-class ListNode{
-    int val;
-    ListNode next;
-    public ListNode(){}
-    public ListNode(int value){val=value;}
-}
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        ListNode smallTail = new ListNode();
-        ListNode smallHead = smallTail;
-        ListNode greatTail = new ListNode();
-        ListNode greatHead = greatTail;
-        ListNode equalTail = new ListNode();
-        ListNode equalHead = equalTail;
+        int[] res = new int[nums.length];
+        int i=0;
         for(int num:nums){
-            ListNode current = new ListNode(num); 
             if(num<pivot){
-                smallTail.next = current;
-                smallTail = smallTail.next;
-            }
-            else if(num>pivot){
-                greatTail.next = current;
-                greatTail = greatTail.next;
-            }
-            else{
-                equalTail.next = current;
-                equalTail = equalTail.next;
+                res[i]=num;
+                i++;
             }
         }
-        smallTail.next = equalHead.next;
-        equalTail.next = greatHead.next;
-        greatTail.next = null;
-        smallHead = smallHead.next;
-        int[] res = new int[nums.length];
-        int i = 0;
-        while(smallHead!=null){
-            System.out.println(smallHead.val);
-            res[i] = smallHead.val;
-            i++;
-            smallHead = smallHead.next;
+        for(int num:nums){
+            if(num==pivot){
+                res[i]=num;
+                i++;
+            }
+        }
+        for(int num:nums){
+            if(num>pivot){
+                res[i]=num;
+                i++;
+            }
         }
         return res;
     }
