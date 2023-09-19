742e874f20bffd2785e33feb20fa3cccf1f3b183
Omar Ihab <47930363+omarihab99@users.noreply.github.com>
2023-09-19 23:38:02 +0300

    Time: 105 ms (49.17%), Space: 75.1 MB (62.5%) - LeetHub

---
+++ 1459-apply-discount-every-n-orders/1459-apply-discount-every-n-orders.java
@@ -0,0 +1,35 @@
+class Cashier {
+    double dis;
+    int n;
+    HashMap<Integer,Integer> p = new HashMap();
+    int[] prices;
+    int c;
+    public Cashier(int n, int discount, int[] products, int[] prices) {
+        this.n=n;
+        dis= (100-discount) / 100.000;
+        prices=prices;
+        c=0;
+        for(int i=0;i<products.length;i++){
+            p.put(products[i], prices[i]);
+        }
+    }
+    public double getBill(int[] product, int[] amount) {
+        c++;
+        int subtotal=0;
+        for(int i=0;i<product.length;i++){
+            subtotal += (p.get(product[i]) * amount[i]);
+        }
+        if(c==this.n){
+            c=0; 
+            return  subtotal * dis;
+        }
+        return (double) subtotal;
+        
+    }
+}
+
+/**
+ * Your Cashier object will be instantiated and called as such:
+ * Cashier obj = new Cashier(n, discount, products, prices);
+ * double param_1 = obj.getBill(product,amount);
+ */
