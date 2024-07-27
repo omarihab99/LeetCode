6e66a991d513f23510facd639a2ca2b779f87c74
Omar Ihab <47930363+omarihab99@users.noreply.github.com>
2024-07-27 18:20:50 +0300

    Time: 100 ms (53.49%), Space: 49.2 MB (6.98%) - LeetHub

---
+++ 0835-linked-list-components/0835-linked-list-components.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public int NumComponents(ListNode head, int[] nums) {
+        HashSet<int> set = new HashSet<int>();
+        bool comp = false;
+        for(int i=0;i<nums.Length;i++){
+            set.Add(nums[i]);
+        }
+        int res = 0;
+        ListNode current = head;
+        while(current!=null){
+            if(set.Contains(current.val)){
+                set.Remove(current.val);
+                comp = true;
+            } else {
+                if(comp) res++;
+                comp = false;
+            }
+            current = current.next;
+        }
+        return res + (comp ? 1 : 0);
+    }
+}
